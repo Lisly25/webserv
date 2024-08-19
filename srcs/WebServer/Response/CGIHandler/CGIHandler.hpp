3b8d33171a288a422c0615a0c9134ada1632109a
Usman Ahmed <uahmed@c2r6p4.hive.fi>
2024-08-19 13:40:43 +0300

    Added CGI handler

--- srcs/WebServer/Response/CGIHandler/CGIHandler.hpp
+++ srcs/WebServer/Response/CGIHandler/CGIHandler.hpp
@@ -12,4 +12,37 @@
 
 #pragma once
 
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <sys/wait.h>
+class Request;
 
+#define PIPES 2
+#define WRITEND 0
+#define READEND 1
+#define CODE404 "404"
+#define CODE500 "500"
+#define PYTHON3 "/bin/python3"
+#define ERROR "\033[31ERROR: \033[0"
+class   CGI
+{
+    public:
+        CGI(Request& request, std::string& respose);
+        ~CGI() = delete;
+
+        std::string&    getCGIResponse( void ) const;
+    private:
+        Request&            _request;
+        std::string&        _response;
+        std::string         _path;
+        int                 _pipe[PIPES];
+        bool    validateExecutable( void );
+        void    executeScript( void );
+        void    child( void );
+        void    parent( pid_t pid );
+        void    setEnvp( char const *envp[] );
+};
