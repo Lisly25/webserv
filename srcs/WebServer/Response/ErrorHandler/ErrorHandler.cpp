4aa4f23e1841846898ef532042a4cbd2aae33415
Szabina Korbai <korbai.szabina@gmail.com>
2024-09-05 11:07:59 +0300

    Added error page for status 400. Added string-form messages for error pages that we do have to ErrorHandler::getErrorMessage()

--- srcs/WebServer/Response/ErrorHandler/ErrorHandler.cpp
+++ srcs/WebServer/Response/ErrorHandler/ErrorHandler.cpp
@@ -34,9 +34,20 @@ std::string ErrorHandler::getErrorMessage(int errorCode) const
     switch (errorCode)
     {
     case 400: return "Bad Request";
+    case 403: return "Forbidden";
     case 404: return "Not Found";
     case 405: return "Method Not Allowed";
+    case 408: return "Request Timeout";
+    case 411: return "Length Required";
+    case 413: return "Payload Too Large";
+    case 431: return "Request Header Fields Too Large";
+    case 501: return "Not Implemented";
+    case 502: return "Bad Gateway";
+    case 503: return "Service Unavailable";
+    case 504: return "Gateway Timeout";
     case 505: return "HTTP Version Not Supported";
+    case 507: return "Insufficient Storage";
+    case 508: return "Loop Detected";
     default: return "Internal Server Error";
     }
 }
