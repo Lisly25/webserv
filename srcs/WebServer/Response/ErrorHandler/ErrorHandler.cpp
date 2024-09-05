7ffc87e031dc89ae78e6bbe2a447331adc4d67d8
Szabina Korbai <korbai.szabina@gmail.com>
2024-09-05 10:45:22 +0300

    The error page is now returned when applicable, not just the path leading to it (Used the readFileContent function from StaticFileHandler)

--- srcs/WebServer/Response/ErrorHandler/ErrorHandler.cpp
+++ srcs/WebServer/Response/ErrorHandler/ErrorHandler.cpp
@@ -9,7 +9,18 @@ void ErrorHandler::handleError(std::string& response) const
 {
     int errorCode = _request.getErrorCode();
     std::string errorMessage = getErrorMessage(errorCode);
-    std::string errorPage = WebParser::getErrorPage(errorCode, _request.getServer());
+    std::string errorPagePath = WebParser::getErrorPage(errorCode, _request.getServer());
+    std::string errorPage;
+    try
+    {
+        readFileContent(errorPagePath, errorPage);
+    }
+    catch(const std::exception& e)
+    {
+        response = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+        return;
+    }
+    
 
     response = "HTTP/1.1 " + std::to_string(errorCode) + " " + errorMessage + "\r\n";
     response += "Content-Type: text/html\r\n";
@@ -28,4 +39,18 @@ std::string ErrorHandler::getErrorMessage(int errorCode) const
     case 505: return "HTTP Version Not Supported";
     default: return "Internal Server Error";
     }
+}
+
+void ErrorHandler::readFileContent(const std::string& path, std::string& content) const
+{
+    std::ifstream fileStream(path, std::ios::in | std::ios::binary);
+    if (!fileStream)
+    {
+        throw std::runtime_error("Failed to read file: " + path);
+    }
+
+    std::ostringstream ss;
+    ss << fileStream.rdbuf();
+    content = ss.str();
+
 }
